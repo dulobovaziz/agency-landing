1fb6d5d995ca397e6884320d6364ddd87a2fb289
dulobovaziz <azizbekpython@gmail.com>
2024-10-26 22:56:52 +0500

    finish the hero section and start services section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,15 @@
 const openBtn = document.querySelector("#sidenavOpenBtn"),
     closeBtn = document.querySelector("#sidenavCloseBtn");
-let  menu = document.querySelector("#menu");
+let  menu = document.querySelector("#menu"),
+    nav = document.querySelector("#nav");
+
+
 openBtn.addEventListener("click", () => {
     menu.style.display = "block";
+    nav.style.position = "absolute"
 })
 closeBtn.addEventListener("click", () => {
     menu.style.display = "none";
+    nav.style.position = "static"
+
 })
